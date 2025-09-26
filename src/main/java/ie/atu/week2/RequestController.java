ab4b162b26db9a6450c6f2b79869008fc54dfab1
JOHNROSSDOHERTY-STUD <dohertyjr0@icloud.com>
2025-09-26 10:30:13 +0100

    added Calculate class and constructor

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -5,19 +5,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping
 public class RequestController {
+    @GetMapping("/hello")
     public String hello(){
         return "Hello";
     }
 
+    @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name){
         return "Hello "+name + "!";
     }
 
+    @GetMapping("/details")
     public String details(@RequestParam String name, @RequestParam int age){
         return "Name:  "+name+", Age "+age+"!";
     }
 
+    @GetMapping("/person")
     public Person getPerson(){
         return new Person("John",23);
     }
+
 }
