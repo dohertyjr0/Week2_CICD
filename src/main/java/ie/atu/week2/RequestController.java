99da0d630a2f857d8f87805da15b7c1002c393d3
JOHNROSSDOHERTY-STUD <dohertyjr0@icloud.com>
2025-09-29 11:13:28 +0100

    modified calc class, created switch

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,6 +1,9 @@
 package ie.atu.week2;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLOutput;
 
 @RestController
 @RequestMapping
@@ -25,5 +28,35 @@ public class RequestController {
         return new Person("John",23);
     }
 
+    @GetMapping("/calculate")
+    public Calculate getOperation(
+            @RequestParam double num1,
+            @RequestParam double num2,
+            @RequestParam String operation){
 
+        double total = 0;
+
+        switch(operation.toLowerCase()){
+            case "add":
+                total = num1 + num2;
+                break;
+
+            case "sub":
+                total = num1 - num2;
+                break;
+
+            case "mul":
+                total = num1 * num2;
+                break;
+
+            case "div":
+                if(num2 == 0){
+                    System.out.println("cannot divide by 0");
+                }
+                else{
+                    total = num1/num2;
+                }
+        }
+        return new Calculate(operation, total);
+    }
 }
